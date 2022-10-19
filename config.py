ba8d36f0419119c2065ed641c733fc6aad6305ce
Andy Ka_ban <ka_ban@rambler.ru>
2022-10-19 21:42:44 +0300

    correct bot end message

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ TELEGRAM_PULL_TIMEOUT = 10
 TELEGRAM_SEND_MSG_TIMEOUT = 7200
 TELEGRAM_DEFAULT_CHAT_IDS = []
 TELEGRAM_BOT_MSG_HEADER = '<b>Новости с пометкой ⚡</b>'
-TELEGRAM_BOT_MSG_END = 'Но бубочка все еще не завел канал про чачу...'
+TELEGRAM_BOT_MSG_END = 'А бубочка все еще не завел канал про чачу...'
 
 # RSS SETTINGS
 RSS_URL = 'https://lenta.ru/rss'
